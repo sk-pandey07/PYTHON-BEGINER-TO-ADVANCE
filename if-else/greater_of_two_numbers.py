1d775336724982dd75276aeaaeba4f657a927096
shashikant kumar <sp8326441@gmail.com>
2026-01-15 20:47:19 +0530

    Added program to find largest of two numbers using if-else

---
+++ if-else/greater_of_two_numbers.py
@@ -0,0 +1,11 @@
+# Problem: Find the greater number between two values
+# Approach: Using if-else condition
+
+
+a = int(input("enter a:"))
+b = int(input("enter b:"))
+
+if(a > b):
+  print("a is greater")
+else:
+  print("b is greater")
