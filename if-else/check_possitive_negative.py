9365a1835b7310a6b93d1dce67dfdcd99d829402
shashikant kumar <sp8326441@gmail.com>
2026-01-15 20:18:01 +0530

    Added positive and negative number check using if-else

---
+++ if-else/check_possitive_negative.py
@@ -0,0 +1,8 @@
+# Problem: Check whether a number is positive or negative
+# Approach: Using if-else condition
+
+num = int(input("enter number:"))
+if(num > 0):
+    print("positive number")
+else:
+    print("negative number")
