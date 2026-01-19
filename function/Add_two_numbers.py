03cda4f40eed76a3fdd3e9d6fa244f43a376df50
shashikant kumar <sp8326441@gmail.com>
2026-01-19 23:19:51 +0530

    Added two sum function program

---
+++ function/Add_two_numbers.py
@@ -0,0 +1,12 @@
+# Problem: Find sum of two numbers using function
+# Approach: Using parameters and return value
+
+def two_sum(a=1,b=2):
+  sum = a + b
+  print(sum)
+  return sum
+
+x = int(input("enter x:"))
+y = int(input("enter y:"))
+
+two_sum(x,y)
