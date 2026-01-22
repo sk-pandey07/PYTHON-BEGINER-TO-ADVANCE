4d3784876f2b1c81fc85737ce2dbba45147bd59a
shashikant kumar <sp8326441@gmail.com>
2026-01-22 23:44:32 +0530

    Added single inheritance example in Python

---
+++ oops/Inheritance.py
@@ -0,0 +1,12 @@
+# Problem: Demonstrate single inheritance in Python
+
+class parent:
+  def skill1(self):
+    print("genetic")
+class child(parent):
+  def skill2(self):
+    print("coding")
+
+c1 = child()
+c1.skill1()
+c1.skill2()
