2610ce0cc361937b1645314c50a88034078d5651
shashikant kumar <sp8326441@gmail.com>
2026-02-03 22:15:23 +0530

    Create Shape_area.py

---
+++ oops/Shape_area.py
@@ -0,0 +1,17 @@
+# Area of Square
+side = 5
+print("Area of Square =", side * side)
+
+# Area of Rectangle
+l = 6
+b = 4
+print("Area of Rectangle =", l * b)
+
+# Area of Circle
+r = 3
+print("Area of Circle =", 3.14 * r * r)
+
+# Area of Triangle
+base = 8
+height = 5
+print("Area of Triangle =", 0.5 * base * height)
